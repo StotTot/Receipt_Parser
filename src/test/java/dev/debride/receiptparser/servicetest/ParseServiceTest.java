a9ab5ee43afcf12d4f449fa700cd9018606d5de4
Aristotle <a.debride.2016@gmail.com>
2021-06-16 12:38:52 -0400

    please work again

--- src/test/java/dev/debride/receiptparser/servicetest/ParseServiceTest.java
+++ src/test/java/dev/debride/receiptparser/servicetest/ParseServiceTest.java
@@ -2,9 +2,7 @@ package dev.debride.receiptparser.servicetest;
 
 import dev.debride.receiptparser.models.Receipt;
 import dev.debride.receiptparser.service.ParseService;
-import dev.debride.receiptparser.service.ParseServiceImpl;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +23,14 @@ public class ParseServiceTest {
     void setUp() throws URISyntaxException, IOException {
         test = parseService.parse("https://images.sampletemplates.com/wp-content/uploads/2018/04/Detailed-Grocery-Payment-Receipt-Samples.jpg");
     }
-//
-//    @Test
-//    void parseData() {
-//        Assertions.assertNotNull(test);
-//    }
-//
-//    @Test
-//    void parseTotal() {
-//        Assertions.assertEquals(46.3, test.getTotal());
-//    }
+
+    @Test
+    void parseData() {
+        Assertions.assertNotNull(test);
+    }
+
+    @Test
+    void parseTotal() {
+        Assertions.assertEquals(46.3, test.getTotal());
+    }
 }
